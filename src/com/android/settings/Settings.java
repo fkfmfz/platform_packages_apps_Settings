fee54279b646a41c454dc70cf4f4bedf8f9d66e8
beanstown106 <nbenis106@gmail.com>
2016-09-25 21:28:13 -0400

    Add Phh Superuser dashboard tile credit to @tsubus for his setting dashboard implementation

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -164,4 +164,5 @@ public class Settings extends SettingsActivity {
     public static class FlashSettingsActivity extends SettingsActivity { /* empty */ }
     public static class SubstratumActivity extends SettingsActivity { /* empty */ }
     public static class SuperSUActivity extends SettingsActivity { /* empty */ }
+    public static class SuperUserActivity extends SettingsActivity { /* empty */ }
 }
